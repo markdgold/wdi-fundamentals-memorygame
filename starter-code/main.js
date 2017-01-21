8fa6b2e770ad805b162f9364b8f082c0077ce285
Mark Goldstein <Mark Goldstein>
2017-01-20 20:36:04 -0700

    added game alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,13 @@ cardOne = "queen";
 cardTwo = "queen";
 cardThree = "king";
 cardFour = "king";
+
+if(cardTwo===cardFour){
+	alert('Sorry, try again.');
+}
+else if(cardOne===cardTwo){
+	alert('You found a match!')
+}
+else if(cardThree===cardFour){
+	alert('You found a match!')
+}
